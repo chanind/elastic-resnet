580b9eb1f58d06d86dc954eb538a2529f76bff60
David Chanin <chanindav@gmail.com>
2021-08-06 12:54:31 +0100

    hopefully fixing running stats tracking

--- elastic_resnet/nn/functional.py
+++ elastic_resnet/nn/functional.py
@@ -10,31 +10,36 @@ def cap_norm(
     running_var: Optional[Tensor] = None,
     training: bool = False,
     momentum: float = 0.1,
-    eps: float = 1e-5,
 ) -> Tensor:
     """
     Batch norm with no weight or bias, which only normalizes variance if it's greater than 1
 
     Philipp, George, and Jaime G. Carbonell. "Nonparametric neural networks." arXiv preprint arXiv:1712.05440 (2017).
     """
-    if training:
-        remove_dims = [dim for dim in range(input.dim()) if dim != 1]
-        target_shape = (1, -1, *[1 for _ in range(input.dim() - 2)])
+    remove_dims = [dim for dim in range(input.dim()) if dim != 1]
+    target_shape = (1, -1, *[1 for _ in range(input.dim() - 2)])
+    var = torch.var(input, dim=remove_dims, unbiased=False).view(target_shape)
+    mean = torch.mean(input, dim=remove_dims).view(target_shape)
+
+    var = running_var
+    mean = running_mean
+
+    if training or var is None:
         var = torch.var(input, dim=remove_dims, unbiased=False).view(target_shape)
+    if training or mean is None:
         mean = torch.mean(input, dim=remove_dims).view(target_shape)
 
-        capped_var = torch.clamp(var, min=1.0)
-        return (input - mean) / torch.sqrt(capped_var)
+    if training:
+        with torch.no_grad():
+            # stats tracking inspired by https://github.com/ptrblck/pytorch_misc/blob/master/batch_norm_manual.py
+            n = input.numel() / input.size(1)
+            if running_mean is not None:
+                running_mean.data = momentum * mean + (1 - momentum) * running_mean.data
+            if running_var is not None:
+                # update running_var with unbiased var
+                running_var.data = (
+                    momentum * var * n / (n - 1) + (1 - momentum) * running_var.data
+                )
 
-    capped_running_var = None
-    if running_var is not None:
-        capped_running_var = torch.clamp(running_var, min=1.0)
-
-    return batch_norm(
-        input,
-        running_mean,
-        capped_running_var,
-        training=training,
-        momentum=momentum,
-        eps=eps,
-    )
+    capped_var = torch.clamp(var, min=1.0)
+    return (input - mean) / torch.sqrt(capped_var)
